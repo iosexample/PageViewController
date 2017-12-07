f39c1fbf7405706c1173412327f5cf3caf1353f8
Dong <donly@magicalboy.com>
2017-12-07 09:26:44 +0800

    Initial Commit

---
+++ PageVC/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  PageVC
+//
+//  Created by dong on 2017/12/7.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
